04cdee775696567c02e819248bded73809ce5890
Nathan Humphreys <nathan.humphreys@tangentsnowball.com>
2014-06-24 10:51:03 +0100

    Add order reference details test

--- tests/gateway_tests.py
+++ tests/gateway_tests.py
@@ -15,3 +15,41 @@ class TestGateway(TestCase):
         self.gateway._do_request = Mock(return_value=mock_responses.STATUS_RESPONSE_XML)
         status = self.gateway.get_service_status()
         self.assertEqual(status, mock_responses.STATUS_RESPONSE_XML)
+
+    def test_get_order_reference_details(self):
+        self.gateway._do_request = Mock(return_value=mock_responses.ORDER_REFERENCE_DETAILS_XML)
+        ref_details = self.gateway.get_order_reference_details()
+        self.assertEqual(ref_details, mock_responses.ORDER_REFERENCE_DETAILS_XML)
+
+    def test_set_order_reference_details(self):
+        pass
+
+    def test_confirm_order_reference(self):
+        pass
+
+    def test_cancel_order_reference(self):
+        pass
+
+    def test_close_order_reference(self):
+        pass
+
+    def test_async_auth(self):
+        pass
+
+    def test_sync_auth(self):
+        pass
+
+    def test_authorization_details(self):
+        pass
+
+    def test_close_authorization(self):
+        pass
+
+    def test_capture(self):
+        pass
+
+    def test_refund(self):
+        pass
+
+    def test_get_refund_details(self):
+        pass
--- tests/mock_responses.py
+++ tests/mock_responses.py
@@ -10,3 +10,38 @@ STATUS_RESPONSE_XML = (
     '</ResponseMetadata>' +
     '</GetServiceStatusResponse>'
 )
+
+ORDER_REFERENCE_DETAILS_XML = (
+    '<GetOrderReferenceDetailsResponse xmlns="http://mws.amazonservices.com/schema/OffAmazonPayments/2013-01-01">' +
+    '<GetOrderReferenceDetailsResult>' +
+    '<OrderReferenceDetails>' +
+    '<AmazonOrderReferenceId>S02-4932843-1071433</AmazonOrderReferenceId>' +
+    '<ExpirationTimestamp>2014-12-21T08:52:38.770Z</ExpirationTimestamp>' +
+    '<Constraints>' +
+    '<Constraint>' +
+    '<ConstraintID>AmountNotSet</ConstraintID>' +
+    '<Description>The seller has not set the amount for the Order Reference.</Description>' +
+    '</Constraint>' +
+    '</Constraints>' +
+    '<IdList/>' +
+    '<Destination>' +
+    '<DestinationType>Physical</DestinationType>' +
+    '<PhysicalDestination>' +
+    '<PostalCode>SE1 5RB</PostalCode>' +
+    '<CountryCode>GB</CountryCode>' +
+    '<City>LONDON</City>' +
+    '</PhysicalDestination>' +
+    '</Destination>' +
+    '<OrderReferenceStatus>' +
+    '<State>Draft</State>' +
+    '</OrderReferenceStatus>' +
+    '<ReleaseEnvironment>Sandbox</ReleaseEnvironment>' +
+    '<SellerOrderAttributes/>' +
+    '<CreationTimestamp>2014-06-24T08:52:38.770Z</CreationTimestamp>' +
+    '</OrderReferenceDetails>' +
+    '</GetOrderReferenceDetailsResult>' +
+    '<ResponseMetadata>' +
+    '<RequestId>e13beadb-c4b1-4c0e-a5b8-b5f8dc2f33ac</RequestId>' +
+    '</ResponseMetadata>' +
+    '</GetOrderReferenceDetailsResponse>'
+)
